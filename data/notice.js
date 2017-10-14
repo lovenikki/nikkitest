3c9c264792088cc578c2f22dc6a2e9d2811049fc
lyun28 <lyun28@gmail.com>
2017-10-14 15:50:13 -0400

    modify

--- data/notice.js
+++ data/notice.js
@@ -2,6 +2,6 @@ var clothesNotice = "";
 
 var clothesHistoryNotice = "";
 
-var levelNotice = "[2017/09/07]Added New Event: Cloud Valentine<br> [2017/09/03]Added Comment Board";
+var levelNotice = "[2017/10/14]Added new suits 'Zashiki' 'Tengu' 'Shutendoji' 'Banshee Momiji'.<br>[2017/09/07]Added New Event: Cloud Valentine<br> [2017/09/03]Added Comment Board";
 
 var levelHistoryNotice = "";
